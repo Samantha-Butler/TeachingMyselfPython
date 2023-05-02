469dd1f7a46a443c6cd3e661b57cff4c95b078ee
JacobStuff <jacobbartlett95@gmail.com>
2023-05-02 21:58:00 +0100

    Changed punctuation

--- Intro and Basics/01 Comments/1what_are_comments.py
+++ Intro and Basics/01 Comments/1what_are_comments.py
@@ -6,5 +6,5 @@
     # 1. As a beginning developer, this helps you write comments above code so you can verbalise what the line of code is doing.
     # 2. If you are picking up a project from someone else, having small comments helps you understand the code faster.
     # 3. Can relay important information or conditions you may need to know before executing the code.
-    # 4. Can hide code and unhide it when you are ready for it to be run in terminal.
+    # 4. Can hide code and unhide it when you are ready for it to be run in terminal.    
 
