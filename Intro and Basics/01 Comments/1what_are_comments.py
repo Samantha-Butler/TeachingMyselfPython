1a47a4fee31c503cb02ed8f7faf8a8f8c31ca130
JacobStuff <jacobbartlett95@gmail.com>
2023-05-02 21:46:45 +0100

    Started intro and basics

---
+++ Intro and Basics/01 Comments/1what_are_comments.py
@@ -0,0 +1,10 @@
+# What are comments? 
+    # Comments are hidden to the console and used for the developers to write small messages needed within the code. 
+    # Comments can be made so long as the '#' is used at the start of the line.
+
+#Benefits of using comments?
+    # 1. As a beginning developer, this helps you write comments above code so you can verbalise what the line of code is doing.
+    # 2. If you are picking up a project from someone else, having small comments helps you understand the code faster.
+    # 3. Can relay important information or conditions you may need to know before executing the code.
+    # 4. Can hide code and unhide it when you are ready for it to be run in terminal.
+
