53c80c202bf61c130b088a376fc5cad6235cdce5
JacobStuff <butlersj1993@gmail.com>
2023-05-10 13:12:39 +0100

    Added echobot project

---
+++ Projects/Simple Echobot.py
@@ -0,0 +1,8 @@
+#This Echobot takes user input in terminal and repeats it!
+
+print("Welcome to Sam's first chatbot!")
+
+print("This chatbox will repeat what you say.")
+stuff_to_echo = input("What would you like to say?")
+
+print("You said: " + stuff_to_echo)
